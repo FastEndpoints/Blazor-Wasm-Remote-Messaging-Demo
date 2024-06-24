9d7e37a0d1bb51ae3a530d4cee7a1bd9ba819932
Kate B <kate@ekbys.com>
2024-06-24 10:33:05 +0530

    Code cleanups

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,22 +1,18 @@
 using bWasm.Client;
 using bWasm.Shared;
+using FastEndpoints;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using FastEndpoints;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+var bld = WebAssemblyHostBuilder.CreateDefault(args);
+bld.RootComponents.Add<App>("#app");
+bld.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new(builder.HostEnvironment.BaseAddress) });
-
-var app = builder.Build();
-
+var app = bld.Build();
 app.Services.MapRemote(
     "https://localhost:7012",
     c =>
     {
         c.Register<CreateOrderCommand, CreateOrderResult>();
     });
-
 await app.RunAsync();
